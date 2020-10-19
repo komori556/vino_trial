6f58ff19963b053f7e1d24d0403a0b7d644e11ca
omori <omori-tomohiro-sd@ynu.jp>
2020-10-19 19:40:47 +0900

    精度の文字の位置がずれていたので修正

--- trial.py
+++ trial.py
@@ -14,11 +14,11 @@ def detect_try(model_name: str = "person-detection-0106"):
     ret, frame = video.read()
     if frame is None:
       raise Exception('Image not found!')
-    _, confidences, boxes = net.detect(frame, confThreshold=0.5)
+    _, confidences, boxes = net.detect(frame, confThreshold=0.3)
     for confidence, box in zip(list(confidences), boxes):
       cv.rectangle(frame, box, color=(0, 255, 0))
       cv.putText(
-        frame, str(confidence[0]), (box[0], box[0] + 15), cv.FONT_HERSHEY_PLAIN, 1, (255, 255, 255),
+        frame, str(confidence[0]), (box[0], box[1] + 15), cv.FONT_HERSHEY_PLAIN, 1, (255, 255, 255),
         1)
     cv.imshow('title', frame)
     cv.waitKey(1)
